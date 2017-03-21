0558f26ed9bf353c231ab32c946a0844175e0899
kunjan <kunjanpatel1@gmail.com>
2017-03-20 22:07:05 -0400

    Pulled variable out of for loop.

--- views/js/main.js
+++ views/js/main.js
@@ -526,6 +526,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+  var movingPizza = document.getElementById("movingPizzas1");
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
@@ -534,7 +535,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+movingPizza..appendChild(elem);
   }
   updatePositions();
 });
