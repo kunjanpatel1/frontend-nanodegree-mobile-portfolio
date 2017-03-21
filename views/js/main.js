b5566c70620129eed7ec5bb37ed81be16b28377f
kunjan <kunjanpatel1@gmail.com>
2017-03-20 21:23:11 -0400

    fixed typo.

--- views/js/main.js
+++ views/js/main.js
@@ -505,7 +505,7 @@ function updatePositions() {
   var scrollTop = Math.sin((document.body.scrollTop / 1250));
 
   for (var i = 0; i < items.length; i++) {
-    var phase = scrollTop + (i % 5));
+    var phase = scrollTop + (i % 5);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
