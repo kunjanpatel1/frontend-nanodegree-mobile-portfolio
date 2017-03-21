a0bcde66bf2aa4c5fe1e90e62dde5bb81ceb6e37
kunjan <kunjanpatel1@gmail.com>
2017-03-20 21:19:45 -0400

    Fixed typo

--- views/js/main.js
+++ views/js/main.js
@@ -502,7 +502,7 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.getElementsByClassName('mover');
-  var scrollTop = Math.sin((document.body.scrollTop / 1250);
+  var scrollTop = Math.sin((document.body.scrollTop / 1250));
 
   for (var i = 0; i < items.length; i++) {
     var phase = scrollTop + (i % 5));
